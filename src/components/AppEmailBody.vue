8b7b3845160ac9f4e31d50cf1a85441acc9a7834
Natella Verde <natellaverde@MacBook-Pro-Natella.local>
2021-04-17 05:58:40 +0300

    vueRouter

---
+++ src/components/AppEmailBody.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="email">
+    <h2>{{  email.theme }}</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi, cumque ea laborum laudantium nostrum odio quis soluta sunt velit?</p>
+    <button class="btn" @click="$router.push('/mail')">Закрыть</button>
+  </div>
+  <div v-else>
+    <h2> Выберете письмо </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['mailId'],
+  inject: ['emails'],
+  computed: {
+    email() {
+      return this.emails.find( e => e.id == this.mailId)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <h1>Page not found</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci dicta esse facere nobis quis?</p>
+    <router-link to="/login">Autorization</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+name: "NotFound"
+}
+</script>
+
+<style scoped>
+
+</style>
